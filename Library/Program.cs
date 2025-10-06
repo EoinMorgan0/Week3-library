3acf736d0580705515f5a36e16ae2a2275d33448
Eoin Morgan <Morgan-E18@ulster.ac.uk>
2025-10-06 13:34:31 +0100

    Updated code with access modifiers

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,29 +1,12 @@
-﻿class Book
+﻿using Library;
+class Program {
+static void Main(string[] args)
 {
-    string Title;
-    string Author;
-    string ISBN;
-    void DisplayInfo()
-    {
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-        Console.WriteLine();
-    }
-    // Example of a constructor that allows us to 'construct' a new
-    // Book object
-    public Book(string bookTitle, string bookAuthor, string bookISBN)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-    }
-    static void Main(string[] args)
-    {
-        // Create a new instance (object) of the Book class
-        // Note how the object name differs from the class name
-        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
+    // Create a new instance (object) of the Book class
+    // Note how the object name differs from the class name
+    Book book = new Book("C# for beginners", "Bill Gates", "1234567");
 
-        book.DisplayInfo();
+    book.DisplayInfo();
     }
+
 }
